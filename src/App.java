c3be616ca87595626134c1f584a93028a67849e9
Zechih <02zechih@gmail.com>
2023-06-13 03:20:58 +0800

    ze

--- src/App.java
+++ src/App.java
@@ -11,19 +11,7 @@ public class App {
         System.out.println("[4] Exit");
         System.out.print("\nNumber: ");
     }
-<<<<<<< HEAD
-=======
-    public static void showstumenu(Student s){
-        System.out.println("\n\nWelcome to the course registration system, " + s.getName());
-        System.out.println("\nKindly select the activity");
-        System.out.println("[1] Register Course");
-        System.out.println("[2] Drop Course");
-        System.out.println("[3] View Course");
-        System.out.println("[4] Exit");
-        System.out.print("\nNumber: ");
-    }
->>>>>>> main
-    
+
     public static void showlecmenu(Lecturer l){
         System.out.println("\n\nWelcome to the course registration system, " + l.getName());
         System.out.println("\nKindly select the activity");
