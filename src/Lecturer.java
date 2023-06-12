99e3c6daf933094dbbf04c874bdbaa299373ca09
“Loy” <tanzenchai0923@gmail.com>
2023-06-12 08:24:19 +0800

    Upload

---
+++ src/Lecturer.java
@@ -0,0 +1,18 @@
+public class Lecturer {
+    private Staff lec;
+    private Course cours;
+    public Lecturer(String n,String i) {
+        this.lec = new Staff(n, i);
+        this.cours = null;
+    }
+    public void setCours(Course cours) {
+        this.cours = cours;
+    }
+    public String getName() {
+        return lec.getName();
+    }
+    public Lecturer() {
+        this.lec = new Staff();
+    }
+
+}
