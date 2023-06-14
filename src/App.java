69b03d27140cc412c5ae84b23f3197b50c3d685f
Zechih <02zechih@gmail.com>
2023-06-14 10:42:28 +0800

    ze

--- src/App.java
+++ src/App.java
@@ -11,29 +11,24 @@ public class App {
         System.out.println("[4] Exit");
         System.out.print("\nNumber: ");
     }
-    public static void showlecmenu(Lecturer l){
-        System.out.println("\n\nWelcome to the course registration system, " + l.getName());
-        System.out.println("\nKindly select the activity");
-        System.out.println("[1] Select Course to teach");
-        System.out.println("[2] View Course");
-        System.out.println("[3] Exit");
-        System.out.print("\nNumber: ");
-    }
+
     public static void main(String[] args) throws Exception {
         ArrayList <Course> courseList = new ArrayList<>();
         ArrayList <Acad> acadList = new ArrayList<>();
         ArrayList <Lecturer> lecList = new ArrayList<>();
         ArrayList <Student> stuList = new ArrayList<>();
+        
         acadList.add(new Acad("Admin A", "S23A1","pass1"));
         acadList.add(new Acad("Admin B", "S23A2","pass2"));
         AController aController = new AController(acadList, courseList, stuList);
         StudentController stuController = new StudentController(stuList,courseList);
-        
+        1
         Lecturer l = new Lecturer("Abdullah", "S33W1");
         lecList.add(l);
         LecturerController lectController = new LecturerController(lecList,courseList); /***** */
 
         Student s = new Student("Loy", "A21ES0223");
+        stuList.add(s);
         Scanner z = new Scanner(System.in);
         int w,y,yss;
         do{
