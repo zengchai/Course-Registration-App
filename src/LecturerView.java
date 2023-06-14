f000a1cf5b1906958fb572dd530ee5355bc0e4ee
lazyyam <yyzhan020623@gmail.com>
2023-06-14 13:04:33 +0800

    asda

--- src/LecturerView.java
+++ src/LecturerView.java
@@ -71,7 +71,7 @@ public class LecturerView {
         System.out.printf("\n%-30s%-10s%-7s%-7s\n","Course Name","Code","Credit","Capity");
         System.out.printf("%-30s%-10s%-7s%-7s\n","-----------","----","------","------");
         for (int i=0;i<c.size();i++){
-            System.out.printf("%-30s%-10s%-7s%-7s\n",c.get(i).getName(),c.get(i).getCode(),c.get(i).getCredit(),c.get(i).getSpace());
+            System.out.printf("%-20s%-13s%-15s%-7s%-7s\n",c.get(i).getName(),c.get(i).getCode(),c.get(i).getLec().getName(),c.get(i).getCredit(),c.get(i).getSpace());
         }
     }
 
@@ -80,7 +80,7 @@ public class LecturerView {
         System.out.printf("\n%-30s%-10s%-7s%-7s\n","Course Name","Code","Credit","Capity");
         System.out.printf("%-30s%-10s%-7s%-7s\n","-----------","----","------","------");
         for (int i=0;i<c.size();i++){
-            System.out.printf("%-30s%-10s%-7s%-7s\n",c.get(i).getName(),c.get(i).getCode(),c.get(i).getCredit(),c.get(i).getSpace());
+            System.out.printf("%-20s%-13s%-15s%-7s%-7s\n",c.get(i).getName(),c.get(i).getCode(),c.get(i).getLec().getName(),c.get(i).getCredit(),c.get(i).getSpace());
         }
     }
 
