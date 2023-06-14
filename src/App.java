97f142512eead79794495e9315c6d0c532c0a502
lazyyam <yyzhan020623@gmail.com>
2023-06-14 08:16:18 +0800

    sds

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class App {
     public static void showmenu(){
-        System.out.println("Course Registration Sysem");
+        System.out.println("Course Registration System");
         System.out.println("\nEnter the number to log-in");
         System.out.println("[1] Student");
         System.out.println("[2] Academic Office Staff");
@@ -38,7 +38,11 @@ public class App {
         acadList.add(new Acad("Admin B", "S23A2","pass2"));
         AController aController = new AController(acadList, courseList);
         StudentController stuController = new StudentController(stuList,courseList);
+        
         Lecturer l = new Lecturer("Abdullah", "S33W1");
+        lecList.add(l);
+        LecturerController lectController = new LecturerController(lecList,courseList); /***** */
+
         Student s = new Student("Loy", "A21ES0223");
         Scanner z = new Scanner(System.in);
         int w,y,yss;
@@ -52,7 +56,7 @@ public class App {
             aController.displayMenu();
         }
         if(w==3){
-            do{
+            /*do{
             showlecmenu(l);
             yss = z.nextInt();
             if(yss == 1){
@@ -61,7 +65,9 @@ public class App {
                 String d = z.nextLine();
             }
             if(yss == 2){
-            }}while(yss!=3);
+            }}while(yss!=3);*/
+
+            lectController.LectControllerMenu();
             
         }
     }while(w!=4);
