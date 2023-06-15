989dd52573b80ed075ab6d37ec126ebaa7d57f6d
zengchai <tanzenchai0923@gmail.com>
2023-06-15 21:25:23 +0800

    Adding feature

--- src/LecturerController.java
+++ src/LecturerController.java
@@ -19,6 +19,7 @@ public class LecturerController {
         clrscr();
         int choice = 0;
         Lecturer lec = lecturerView.confirmLecturer();
+
          for (int a = 0 ; a<lecturerList.size(); a++){
             if(lec.getName().equals(lecturerList.get(a).getName())&&
             lec.getLectID().equals(lecturerList.get(a).getLectID())) {
@@ -38,13 +39,13 @@ public class LecturerController {
             switch (choice){
                 case 1:  
                 clrscr();
-                this.chooseTeachingCourse(lecturerView);
+                this.chooseTeachingCourse();
                 lecturerView.requestProceed();
                 break;
 
                 case 2: 
                 clrscr();
-                this.removeTeachingCourse(lecturerView);
+                this.removeTeachingCourse();
                 lecturerView.requestProceed();
                 break;
 
@@ -58,29 +59,39 @@ public class LecturerController {
         }
     }
 
-    public void chooseTeachingCourse(LecturerView lecturerView){
+    public void chooseTeachingCourse(){
         lecturerView.displayTeachingCourse(courseList);
         String temp = lecturerView.showTeachingCourse();
         boolean repeat = false;
         boolean clash = false;
+        int z = 0;
 
-        for(int i=0;i<lecturerList.get(index).getSelectedCourse().size();i++)
-        { 
-            if (temp.equals(lecturerList.get(index).getSelectedCourse().get(i).getCode()))
+        for(int i=0;i<courseList.size();i++)
+        {   
+            if (temp.equals(courseList.get(i).getCode()))
             {
-                repeat = true;
-                lecturerView.displayRepeatTrue();
-            }
+                if (!courseList.get(i).getLec().getName().equals(""))
+                {
+                    clash = true;
+                    lecturerView.displayClash();
+                    break;
+                }
+
+                
+        }
         }
 
-        for(int i=0;i<lecturerList.get(index).getSelectedCourse().size();i++)
-        { 
-            if (!(lecturerList.get(index).getSelectedCourse().get(i).getLec().getName().equals(courseList.get(i).getLec().getName())))
-            {
-                clash = true;
-                lecturerView.displayClash();
-            }
-        }
+        // If the lecture teach the same course
+        // for(int s=0;s<lecturerList.get(index).getSelectedCourse().size();s++)
+        //     { 
+        //         if (temp.equals(lecturerList.get(index).getSelectedCourse().get(s).getCode()))
+        //         {
+        //             repeat = true;
+        //             lecturerView.displayRepeatTrue();
+        //         }
+        //     }
+
+        
 
         if(repeat == false && clash == false){
             for (int i =0;i<courseList.size();i++){
@@ -88,30 +99,40 @@ public class LecturerController {
                     lecturerList.get(index).getSelectedCourse().add(courseList.get(i)); //inside lect will have teached course
                     courseList.get(i).setLec(lecturerList.get(index)); //set teaching lec name
                     lecturerView.chooseSuccessful(temp);
-                
+                    z = 0;
+                    break;
                 }
                 else{
-                    lecturerView.registerfail(temp);
-
+                    z = 1;
                 }
+            }
+            if(z==1){
+                lecturerView.registerfail(temp);
             }
         }
     }   
 
-    public void removeTeachingCourse(LecturerView lecturerView){
+    public void removeTeachingCourse(){
         viewSelectedTeachingCourse();
         String temp = lecturerView.showDeleteTeachingCourse();
+        int z = 0;
 
         for (int i =0;i<courseList.size();i++){ 
             if(temp.equals(courseList.get(i).getCode())){
                 lecturerList.get(index).getSelectedCourse().remove(courseList.get(i));
                 lecturerView.removeSuccessful(temp);
-
+                courseList.get(i).setLec(new Lecturer());; //set teaching lec name
+                z = 0;
+                break;
             }
             else{
-                lecturerView.removefail(temp);
+                z = 1;
             }
-        }   
+        }
+        
+        if (z == 1){
+            lecturerView.removefail(temp);
+        }
     }
 
     public void viewSelectedTeachingCourse(){
